d4beda8648792efcc32a803b54a0048a04c641d5
Rold Lorenzo <lorenzo@laboware.com.br>
2024-11-26 15:25:10 -0300

    adicionadas as classes para a ast representar expressoes

--- run_tests.py
+++ run_tests.py
@@ -3,7 +3,8 @@ import unittest
 import lexer.tests
 
 if __name__ == "__main__":
-    suite = unittest.defaultTestLoader.loadTestsFromModule(lexer.tests)
-    
     runner = unittest.TextTestRunner()
-    runner.run(suite)
+    
+    testes_lexer = unittest.defaultTestLoader.loadTestsFromModule(lexer.tests)
+    
+    runner.run(testes_lexer)
